23f1893c764588713c6b853492c848bdb8794412
Joao Ferreira <jferreira@nomadvantage.com>
2017-03-10 14:29:34 +0100

    handle error on login

--- src/oauth-service.ts
+++ src/oauth-service.ts
@@ -267,6 +267,12 @@ export class OAuthService {
         
         var parts = this.getFragment();
 
+        if (parts["error"]) {
+            console.debug('error trying to login');
+            this.handleLoginError(options, parts);
+            return false;
+        }
+
         var accessToken = parts["access_token"];
         var idToken = parts["id_token"];
         var state = parts["state"];
@@ -330,6 +336,13 @@ export class OAuthService {
         
         return true;
     };
+
+    handleLoginError(options: any, parts: any): void {
+        var savedNonce = this._storage.getItem("nonce");
+        if (savedNonce === parts.state && options.onLoginError)
+            options.onLoginError(parts)
+        if (this.clearHashAfterLogin) location.hash = '';
+    }
     
     processIdToken(idToken, accessToken) {
             var tokenParts = idToken.split(".");
