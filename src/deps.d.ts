6e269e24e716da86dd78ab3920a7e89249d039de
ManfredSteyer <manfred.steyer@gmx.net>
2017-03-30 19:04:46 +0200

    deps.d.ts

---
+++ src/deps.d.ts
@@ -0,0 +1,12 @@
+declare module "sha256" {
+    export var sha256: any;
+    export default sha256;  
+} 
+
+declare module "js-base64" {
+    export var Base64: any;
+}
+
+declare module "base64-js" {
+    export var fromByteArray: any;
+}
