8c4880e8ef1905c0c1c35cddb1cb083fc042ec8c
ManfredSteyer <manfred.steyer@gmx.net>
2017-09-14 00:16:30 +0200

    cleanup

--- sample - Kopie/src/app/app.tokens.ts
+++
@@ -1,3 +0,0 @@
-import {OpaqueToken} from "@angular/core";
-
-export const BASE_URL = new OpaqueToken("BASE_URL");
--- sample - Kopie/src/app/flight-booking/flight-booking.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import {FlightService} from "./services/flight.service";
-
-@Component({
-    selector: 'flight-booking',
-    templateUrl: './flight-booking.component.html'
-})
-export class FlightBookingComponent {
-}
