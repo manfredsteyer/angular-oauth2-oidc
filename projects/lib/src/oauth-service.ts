c242db140688014aa716f49741af4599b7def22b
Flofie <fiedlefl@gmail.com>
2020-09-24 10:45:16 +0200

    Update oauth-service.ts

--- projects/lib/src/oauth-service.ts
+++ projects/lib/src/oauth-service.ts
@@ -2544,12 +2544,16 @@ export class OAuthService extends AuthConfig implements OnDestroy {
       );
     }
     
+    let addParams = {};
     let loginHint = null;
     if (typeof params === 'string') {
         loginHint = params;
     }
+    else if (typeof params === 'object') {
+        addParams = params;
+    }
 
-    this.createLoginUrl(additionalState, loginHint, null, false, params)
+    this.createLoginUrl(additionalState, loginHint, null, false, addParams)
       .then(this.config.openUri)
       .catch(error => {
         console.error('Error in initAuthorizationCodeFlow');
